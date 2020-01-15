70f3d273ac08bb09b9e5380b0b18d569c9f7fabc
Lion.M <lionem2018@gmail.com>
2020-01-15 19:15:37 +0900

    [20200115] Solve 1330

---
+++ C++/1330_C.cpp
@@ -0,0 +1,24 @@
+/**
+ * 2018/08/09
+ * 백준 1330 번 문제: 두 수 비교하기(https://www.acmicpc.net/problem/1330)
+ * 언어: C++
+*/
+
+#include <iostream>
+
+int main(void)
+{
+	int A, B;
+
+	scanf_s("%d", &A);
+	scanf_s("%d", &B);
+
+	if (A > B)
+		printf(">");
+	else if (A < B)
+		printf("<");
+	else
+		printf("==");
+
+	return 0;
+}
